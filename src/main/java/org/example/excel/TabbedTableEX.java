d754e2127383c534967307432f782f7879bde438
WooYong <jwooy00@naver.com>
2023-06-03 14:58:21 +0900

    TabbedTable 수정

--- src/main/java/org/example/excel/TabbedTableEX.java
+++ src/main/java/org/example/excel/TabbedTableEX.java
@@ -1,19 +1,22 @@
 package org.example.excel;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+
 import java.net.Socket;
+
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Set;
-import javax.swing.ImageIcon;
+import java.lang.*;
 
 
-public class TabbedTable extends JFrame implements ActionListener {
+
+public class TabbedTableEX extends JFrame implements ActionListener {
     // 교과서 497p에 있는 JTabbedPane 사용. 인기메뉴/ 면류/ 밥류 등등..
     private BufferedReader in = null;
     private BufferedWriter out = null;
@@ -24,39 +27,37 @@ public class TabbedTable extends JFrame implements ActionListener {
     private JTabbedPane pane = new JTabbedPane(JTabbedPane.LEFT);
     private JTabbedPane text = new JTabbedPane(JTabbedPane.RIGHT);
     private JTextArea basketText = new JTextArea();
+    private JTextArea orderText = new JTextArea();
     private JTextArea billText = new JTextArea();
-    private JTextArea last = new JTextArea();
 
     // 인기메뉴에 해당하는 Panel
     private JPanel popular = new JPanel();
     private JPanel[][] p;
     private JButton[][] plus, minus, basket;
-    private JButton getbasket,canclebasket,total;
+    private JButton getbasket,canclebasket,total, cancleorder;
     private JLabel[][] lWon,lmenu, count1;
-    private org.example.excel.orderEX orderEX;
     public int number = orderEX.getCount();
-    int totalsum =0;
-    public TabbedTable() {
+
+    public TabbedTableEX() {
         // 타이틀 제목에 테이블 번호를 입력함
         setTitle("Table" + "명지 주문 시스템 테이블 번호 " + number);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         receiver = new Receiver(); // 서버에서 받은 메시지를 출력할 컴퍼넌트
+        receiver.setEditable(false); // 편집 불가
 
-        String output = null;
-        sender = new JTextField("고객요청사항: ");
+        sender = new JTextField();
         sender.addActionListener(this);
 
-        text.add(new JScrollPane(receiver),"주문내역"); // 스크롤바를 위해  ScrollPane 이용
-        text.add(sender,"고객요청사항");
+        text.add(new JScrollPane(receiver),BorderLayout.CENTER); // 스크롤바를 위해  ScrollPane 이용
+        text.add(sender,BorderLayout.SOUTH);
 
         //버튼 개수와 그리드 레이아웃 크기 설정
         int numCols = 3;
         int numRows = 3;
-        int size =10;
         final int[] count = {0};
         //GridLayout 으로 설정. 3,3 5는 픽셀단위
-        popular.setLayout(new GridLayout(numRows, size, 5, 5));
+        popular.setLayout(new GridLayout(numRows, numCols, 5, 5));
 
         //버튼 배열 생성
         p = new JPanel[numRows][numCols];
@@ -69,9 +70,10 @@ public class TabbedTable extends JFrame implements ActionListener {
         getbasket = new JButton("주문하기");
         canclebasket = new JButton("취소");
         total = new JButton("총 금액 표시");
+        cancleorder = new JButton("주문취소");
 
 
-        LinkedHashMap<String, String> price = new LinkedHashMap<>();
+        HashMap<String,String> price = new HashMap<String,String>();
         price.put("10000","제육덮밥");
         price.put("9000", "고추장찌개");
         price.put("8000","새우볶음밥");
@@ -81,36 +83,15 @@ public class TabbedTable extends JFrame implements ActionListener {
         price.put("4000","라면");
         price.put("4500","떡만두국");
         price.put("6500","짜장면");
-        price.put("25000","해물찜");
-
-        LinkedHashMap<String, String> imagePaths = new LinkedHashMap<>();
-        imagePaths.put("10000", "c:/jangu.jpg");
-        imagePaths.put("9000", "c:/jangu.jpg");
-        imagePaths.put("8000", "c:/jangu.jpg");
-        imagePaths.put("11000", "c:/jangu.jpg");
-        imagePaths.put("7000", "c:/jangu.jpg");
-        imagePaths.put("6000", "c:/jangu.jpg");
-        imagePaths.put("4000", "c:/jangu.jpg");
-        imagePaths.put("4500", "c:/jangu.jpg");
-        imagePaths.put("6500", "c:/jangu.jpg");
-        imagePaths.put("25000", "c:/jangu.jpg");
         //교과서 414p 에 있는 Iterator 를 사용함.
         Set<String> keys = price.keySet();
         Iterator<String> it = keys.iterator();
-
 
 
         for(int i=0; i<numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 p[i][j] = new JPanel();
                 String key = it.next();
-                String imagePath = imagePaths.get(key);
-                ImageIcon imageIcon = new ImageIcon(imagePath);
-                Image img = imageIcon.getImage();
-                Image sizeImage = img.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-                ImageIcon changeIcon = new ImageIcon(sizeImage);
-                JLabel imageLabel = new JLabel(changeIcon);
-
                 // 값을 넣기 위해서 Iterator를 사용한 것.
                 count1[i][j] = new JLabel("1");
                 lWon[i][j] = new JLabel(key );
@@ -127,7 +108,6 @@ public class TabbedTable extends JFrame implements ActionListener {
                 p[i][j].add(basket[i][j]);
                 p[i][j].add(lWon[i][j]);
                 p[i][j].add(lmenu[i][j]);
-                p[i][j].add(imageLabel);
             }
         }
         // + 누르면 숫자 감소
@@ -180,21 +160,15 @@ public class TabbedTable extends JFrame implements ActionListener {
                     break;
                 else {
                     basket[i][j].addActionListener(new ActionListener() {
+
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             String menu = lmenu[row][col].getText(); // Use the captured values
                             String won = lWon[row][col].getText();
                             int count = Integer.parseInt(count1[row][col].getText());
 
-                            int price = Integer.parseInt(won);
-                            int sum = price * count;
-                            totalsum += sum;
-
                             String output = String.format("%s  %s원  %d 개\n", menu, won, count);
-
                             basketText.append(output);
-                            last.setText("총 가격 : " + totalsum );
-
                         }
                     });
                 }
@@ -202,39 +176,18 @@ public class TabbedTable extends JFrame implements ActionListener {
         }
         //장바구니
         JPanel buy = new JPanel();
-        basketText.setText("");
+        basketText.setText("테이블 번호 " + number + "\n\n");
         buy.add(new JScrollPane(basketText));
         buy.add(getbasket);
         buy.add(canclebasket);
         buy.add(total);
 
-        //주문하기 버튼 리스너
-        getbasket.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String call = basketText.getText(); // 텍스트 필드에 사용자가 입력한 문자열
-                try {
-                    out.write("테이블 번호 " + number + "\n" + call); // 문자열 전송
-                    ;
-                    receiver.append(call);
-                    billText.append(call);
-
-
-                    out.flush();
-
-
-                    basketText.setText("");
-
-                } catch (IOException e1) {
-                    handleError(e1.getMessage());
-                }
-            }
-        });
+        getbasket.addActionListener(this);
 
         canclebasket.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                basketText.setText("테이블 번호 " + number + "\n\n");
+                basketText.setText("테이블 번호 " + number + "\n");
             }
         });
 
@@ -247,27 +200,39 @@ public class TabbedTable extends JFrame implements ActionListener {
         Thread th = new Thread(receiver); // 상대로부터 메시지 수신을 위한 스레드 생성
         th.start();
 
+        //주문내역
+        JPanel order = new JPanel();
+        order.add(new JScrollPane(orderText));
+        order.add(cancleorder);
+
+        //주문취소 버튼 누를 시 동작
+        cancleorder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //주문내역 텍스트필드 비우기
+                orderText.setText("");
+                //주문내역 소켓으로 취소 보내야함
+
+                //계산서 텍스트필드 비우기
+                billText.setText("");
+            }
+        });
+
         //계산서
         JPanel bill = new JPanel();
         bill.add(new JScrollPane(billText));
-        bill.add(new JScrollPane(last));
-        billText.setText("테이블 번호: " + number +" 계산서 \n\n");
-        last.setText("총 가격: " + totalsum);
-
 
         // private JTabbedPane pane = new JTabbedPane(JTabbedPane.LEFT);
         // BorderLayout의 왼쪽에 추가.
-        JScrollPane popularScroll = new JScrollPane(popular);
-        popularScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  // 수직 스크롤
-        popular.setPreferredSize(new Dimension(1000, 600)); // 원하는 크기로 설정
-        pane.addTab("인기 메뉴", popularScroll);
+        pane.addTab("인기 메뉴", popular);
         pane.add("장바구니", buy);
+        pane.add("주문내역", order);
         pane.add("계산서", bill);
         add(pane, BorderLayout.WEST);
         add(text, BorderLayout.EAST);
 
 
-        setSize(1450, 600);
+        setSize(1200, 600);
         setVisible(true);
     }
     private class Receiver extends JTextArea implements Runnable {
@@ -294,29 +259,24 @@ public class TabbedTable extends JFrame implements ActionListener {
         if (e.getSource() == sender) {
             String msg = sender.getText(); // 텍스트 필드에 사용자가 입력한 문자열
             try {
-                if (msg.startsWith("고객요청사항:")) {
-                    msg = msg.substring(8); // "고객요청사항:" 부분을 제거하여 전송
-                }
-
-                out.write("테이블 번호:" + number+ "\n"+"고객요청사항: " + msg + "\n"); // 문자열 전송
+                out.write(msg+"\n"); // 문자열 전송
                 out.flush();
 
+                receiver.append("\n클라이언트 : " + msg); // JTextArea에 출력
                 int pos = receiver.getText().length();
                 receiver.setCaretPosition(pos); // caret 포지션을 가장 마지막으로 이동
-                sender.setText("고객요청사항: ");
-
+                sender.setText(null); // 입력창의 문자열 지움
             } catch (IOException e1) {
                 handleError(e1.getMessage());
             }
         }
-        //주문하기 버튼
         else if (e.getSource() == getbasket) {
-            String call = basketText.getText();
-
+            String call = basketText.getText(); // 텍스트 필드에 사용자가 입력한 문자열
             try {
-                out.write("테이블 번호:" + number+ "\n"+call+"\n"); // 문자열 전송
+                orderText.setText(call);
+                billText.setText(call);
+                out.write(call+"\n"); // 문자열 전송
                 out.flush();
-
 
                 sender.setText(null); // 입력창의 문자열 지움
             } catch (IOException e1) {
@@ -328,7 +288,7 @@ public class TabbedTable extends JFrame implements ActionListener {
     private void setupConnection() throws IOException {
         socket = new Socket("localhost", 9999); // 클라이언트 소켓 생성
         // System.out.println("연결됨");
-        receiver.append("주문내역"+"\n");
+        receiver.append("서버에 연결 완료");
         int pos = receiver.getText().length();
         receiver.setCaretPosition(pos); // caret 포지션을 가장 마지막으로 이동
 
@@ -336,6 +296,6 @@ public class TabbedTable extends JFrame implements ActionListener {
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // 클라이언트로의 출력 스트림
     }
     public static void main(String[] args) {
-        new TabbedTable();
+        new TabbedTableEX();
     }
 }
